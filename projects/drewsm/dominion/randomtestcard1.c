7045d422ad6dee937c1fe592de9e68ccabc5f53a
mel <mel@localhost.localdomain>
2018-03-04 17:16:23 -0500

    minor changes in randomtestcard1.c

--- projects/drewsm/dominion/randomtestcard1.c
+++ projects/drewsm/dominion/randomtestcard1.c
@@ -21,10 +21,7 @@ int main() {
     int smithyCards1, smithyCards2;
     int currentPlayer;
     int failures = 0;
-    
-    //Tracking for current values
-    
-        
+            
     //Test result explanation strings
     char *param0 = "smithy card";
     char *param1 = "pre-test handCount";
